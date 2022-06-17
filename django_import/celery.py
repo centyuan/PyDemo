9da5d695830d328acbd03caaec9506b387cd7865
centyuan <yuanhuailin@i-soon.net>
2022-06-17 20:04:48 +0800

    modify something

--- django_import/celery.py
+++ django_import/celery.py
@@ -6,7 +6,6 @@ app = Celery('hello',broker='amqp://guest@localhost//')
 def hello():
     return "hello world"
 
-
 # 调用方式
 result = hello.delay()
 hello.apply_async()
@@ -19,5 +18,13 @@ https://docs.celeryq.dev/en/master/userguide/monitoring.html
 pip install flower
 celery -A proj flower 或celery -A proj flower --port=5555
 open http://localhost:5555
+
+
+#任务调用
+# 方法一：delay方法
+task_name.delay(args1, args2, kwargs=value_1, kwargs2=value_2)
+​
+# 方法二： apply_async方法，与delay类似，但支持更多参数
+task.apply_async(args=[arg1, arg2], kwargs={key:value, key:value})
 """
 
--- python实例/python selenium.py
+++ python实例/python selenium.py
@@ -5,11 +5,12 @@
 from selenium import webdriver
 import time
 
-#help(webdriver)支持那些浏览器
+# help(webdriver)支持那些浏览器
 broswer=webdriver.Chrome(executable_path=r"D:\python_data\centyuan\cent30\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe")
-#driver1=webdriver.Chrome()
+# driver1=webdriver.Chrome()
 broswer.get('https://www.baidu.com')#打开页面
-#driver1.get('https://www.google.com')
+# driver1.get('https://www.google.com')
+
 """
 定位节点方法
 find_element_by_id()	通过 id 属性值定位
@@ -26,7 +27,7 @@ input = broswer.find_element_by_css_selector('#kw')  # 通过CSS id 选择器获
 input.send_keys("海南师范大学")#输入
 # input.clear() 清空内容
 # input.text 获取文本的值
-#print(driver.page_source)  #获取源代码
+# print(driver.page_source)  #获取源代码
 print(broswer.current_url)#获取请求链接
 broswer.get_cookies()
 button = broswer.find_element_by_css_selector('#su')  # 通过CSS id 选择器获取响应元素 #
@@ -36,3 +37,14 @@ broswer.execute_script(
 )
 time.sleep(5)
 broswer.close() #关闭浏览器
+
+if __name__ == '__main__':
+      # 不打开浏览器
+      option = webdriver.ChromeOptions()
+      option.add_argument('headless')  # 设置option
+      driver = webdriver.Chrome(
+            executable_path=r"D:\python_data\centyuan\cent30\Lib\site-packages\selenium\webdriver\chrome\chromedriver.exe",
+            options=option)  # 调用带参数的谷歌浏览器
+      driver.get("http://www.taobao.com")
+      print(driver.page_source)  # 获取动态页面
+
