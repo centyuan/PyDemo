2eee4a6713e7766a4b9107a99a668ad0b2e5acab
centyuan <yuanhuailin@i-soon.net>
2022-09-21 16:06:59 +0800

    大文件断点续传

--- django_import/django 文件下载or上传.py
+++ django_import/django 文件下载or上传.py
@@ -1,4 +1,6 @@
 # 1.HttpResponse
+import os
+
 from django.http import HttpResponse, StreamingHttpResponse, FileResponse
 
 
@@ -26,7 +28,7 @@ def file_download(request):
 def file_download(request):
     response = FileResponse(open('ssr.txt', 'rb'))
     # as_attachment:是否提供文件下载还是在浏览器读取
-    response = FileResponse(open('ssr.txt', 'rb'),as_attachment=True,filename='文件名.txt')
+    response = FileResponse(open('ssr.txt', 'rb'), as_attachment=True, filename='文件名.txt')
     response['content_type'] = "application/octet-stream"
     response['Content-Disposition'] = 'attachment; filename=' + 'ssr.txt'
     return response
@@ -64,7 +66,7 @@ print('返回response', response, response['Content-Disposition'])
 return response
 """
 
-# django 文件上传
+# 4.django 文件上传
 """
 
 http://c.biancheng.net/view/8120.html
@@ -74,14 +76,89 @@ if request.FILES.getlist('filename'):
         program = Program.objects.create(case=case, program=file, program_name=file.name)
         # file.name 文件名 file 文件内容
 单一文件上传:
-file = request.FILES['myfile']  # 获取文件流对象file
+# 获取文件流对象UploadedFile对象 (django.core.files.uploadedfile.UploadedFile)
+# UploadedFile.read（）：从文件中读取整个上传的数据。小心整个方法：如果这个文件很大，你把它读到内存中会弄慢你的系统。你可以想要使用chunks（）来代替，看下面；
+# UploadedFile.chunks()：如果上传的文件足够大需要分块就返回真。默认的这个值是2.5M，当然这个值是可以调节的。
+file = request.FILES['myfile']  
 # file = request.FILES.get('myfile') 
 filename = os.path.join(settings.MEDIA_ROOT,file.name) # 文件储存路径，应用settings中的配置，file.name获取文件名
+# 方式1
 with open(filename,'wb') as f:
-    data = file.file.read()
+    data = file.read()
     f.write(data)
-    
+# 方式2
+with open(filename,'wb') as f:
+    for chunk in file.chunks():
+        f.write(chunk)
+
 warning:
 1.POST方法
 2.request的<form>有属性enctype="multipart/form-data"
 """
+
+
+# 5.django 大文件断点上传和下载
+# 一:断点文件上传
+# 刪除目录及以下文件
+def deldir(dir):
+    if not os.path.exists(dir):
+        return False
+    if os.path.isfile(dir):
+        os.remove(dir)
+        return
+    for i in os.listdir(dir):
+        t = os.path.join(dir, i)
+        if os.path.isdir(t):
+            deldir(t)  # 递归调用
+        else:
+            os.unlink(t)  # 删除文件
+    os.removedirs(dir)  # 递归删除目录下面的空文件夹
+
+
+# 1.上传分片
+# https://blog.csdn.net/qq_42991509/article/details/126589444
+def upload_chunk(request):
+    file_name = request.POST.get('identifier')
+    chunk_index = int(request.POST.get('chunknumber'))
+    total_chunk = int(request.POST.get('totalchunks'))
+    upload_file = request.FILES['myfile']
+    file_path = os.path.join('chunk_file', file_name)
+    chunk_path = os.path.join(file_path, chunk_index)
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+    with open(chunk_path, 'wb+') as destination:
+        for chunk in upload_file.chunks():
+            destination.write(chunk)
+
+
+# 2.合并分片
+def merge_chunk(chunk_file_path, total_chunk):
+    """
+    :param chunk_file_path: 分片目录
+    :param total_chunk: 总分片数
+    """
+    chunks_file = list(set(os.listdir(chunk_file_path)))
+    if len(chunks_file) == total_chunk:
+        # 所有的分片 必须按照分块顺序排序，否则 可能合并的文件顺序被打乱
+        all_chunk_file = os.listdir(chunk_file_path)
+        all_chunk_file.sort(key=lambda x: int(x))
+        target_path = os.path.join('chunk_file', '原始文件名.zip')
+        with open(target_path, 'wb+') as f:
+            for chunk_ in all_chunk_file:
+                chunk_path = os.path.join(chunk_file_path, chunk_)
+                with open(chunk_path, 'wb+') as g:
+                    data = g.read()
+                    f.write(data)
+
+# 二:断点文件下载
+# https://blog.csdn.net/ALLENsakaru/article/details/109039465 
+# 1.文件生成器,防止文件过大，内存溢出
+def down_file_iterator(file_path,start_pos,chunk_size):
+    with open(file_path,mode='rb') as f:
+        f.seek(start_pos,os.SEEK_SET)
+        content = f.read(chunk_size)
+        yield content
+
+# 2.断点下载
+def breakpoint_download(request,filename):
+    pass
--- django_import/path_urls.py
+++ django_import/path_urls.py
@@ -1,5 +1,6 @@
 from django.urls import path,re_path
 views = None
+# https://zhuanlan.zhihu.com/p/105570722
 urlpatterns = [
     path('rank/<int:number>',views),
     re_path('rank/(?P<pk>[0-9]+)?',views)
