19ba9361e705c628a570be160f16604a69124ee5
centyuan <yuanhuailin@i-soon.net>
2022-10-19 08:45:39 +0800

    modify textin_ocr

--- python实例/ocr/textin_ocr.py
+++ python实例/ocr/textin_ocr.py
@@ -1,13 +1,33 @@
+import os
 import requests
 
-
 class TextinApi:
-    def __init__(self,api_key,secret_key):
+    def __init__(self, api_key, secret_key):
         self.url = 'https://api.textin.com/ai/service/v2/recognize'
         self.head = {
-            'x-ti-app-id':api_key,
-            'x-ti-secret-code':secret_key,
+            'x-ti-app-id': api_key,
+            'x-ti-secret-code': secret_key,
         }
 
-    def get_text(self,file_path=None,file_url=None):
-        pass
+    def get_text(self, file_path=None, file_url=None):
+        try:
+            if os.path.exists(file_path):
+                image = None
+                with open(file_path, 'rb') as f:
+                    image = f.read()
+
+                resp = requests.post(self.url, data=image, headers=self.head)
+                result = resp.json()['result']['lines'][0]
+                return True, result['text']
+            else:
+                return False, '文件为空'
+        except Exception as e:
+            return False, e
+
+
+if __name__ == '__main__':
+    api_key = '20dca87e2e299c49307c10fdc3624199'
+    secret_key = '2399c33699ede20b8034717c8f65e74b'
+    client = TextinApi(api_key=api_key,secret_key=secret_key)
+    mark, text = client.get_text(file_path='resource/DYEA.png')
+    print(text)
