ff961f74fb8a823c2883f8cbe71f0daa1cba0fb8
centyuan <yuanhuailin@i-soon.net>
2022-10-19 20:44:56 +0800

    no message

--- python实例/ocr/test.py
+++ python实例/ocr/test.py
@@ -1,5 +1,8 @@
+from DecryptLogin import login
 
-
-for item in range(0):
-    print('aa')
-print('b')
+# the instanced client
+client = login.Client()
+# the instanced weibo
+weibo = client.weibo(reload_history=True)
+# use the login function to login in weibo
+infos_return, session = weibo.login('me', 'pass', 'scanqr')
--- python实例/ocr/textin_ocr.py
+++ python实例/ocr/textin_ocr.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 class TextinApi:
-    def __init__(self, api_key, secret_key):
+    def __init__(self, api_key, secret_key,level=1):
         self.url = 'https://api.textin.com/ai/service/v2/recognize'
         self.head = {
             'x-ti-app-id': api_key,
