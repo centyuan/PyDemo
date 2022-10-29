b1b3f6e4fd1d553d0f06a843602e1fbf8e88a2e1
centyuan <yuanhuailin@i-soon.net>
2022-10-29 11:49:48 +0800

    no message

--- python特性/datetime and time.py
+++ python特性/datetime and time.py
@@ -2,27 +2,27 @@
 import time
 import calendar
 import datetime
+from wsgiref.handlers import format_date_time
 # 1.时间time
 t = time.time()
 print('time.time():', time.time())
-print(t)                        # 原始时间数据--1970年是距今最早的的1月1日是星期一的日期
-print(int(t))                   # 秒级时间戳
-print(int(round(t * 1000)))     # 毫秒级时间戳
-print(int(round(t * 1000000)))  # 微秒级时间戳
+print('时间戳:',t)  # 原始时间数据--1970年是距今最早的的1月1日是星期一的日期
+print('秒级时间戳:',int(t))  # 秒级时间戳
+print('毫秒级时间戳:',int(round(t * 1000)))  # 毫秒级时间戳
+print('微秒级时间戳:',int(round(t * 1000000)))  # 微秒级时间戳
 print('time.localtime():', time.localtime())
 time_classS = time.localtime()
 print(type(time_classS), time_classS.tm_year)
 print('time.asctime()：', time.asctime())
 
 # 2.时间元组
-print('时间元组：', time.localtime(time.time()))
+print('时间元组:', time.localtime(time.time()))
 # 3.可读的格式化时间
 print('可读的格式化时间：', time.asctime(time.localtime(time.time())))
 print('time.strftime--%Y-%m-%d %H:%M:%S:', time.strftime("%Y-%m-%d %H:%M:%S"))
 print('time.strftime--%a %b %d %H:%M:%S %Y:', time.strftime("%a %b %d %H:%M:%S %Y"))
 # 4.字符串转为日期
-text = '2022-09-20'
-print('字符串转为日期：', datetime.datetime.strptime(text, "%Y-%m-%d"))  # 性能很差
+print('字符串转为日期：', datetime.datetime.strptime('2022-09-20', "%Y-%m-%d"))  # 性能很差
 print("以下输出2016年1月份的日历:")
 print(calendar.month(2018, 2))
 
@@ -35,10 +35,11 @@ print(calendar.month(2018, 2))
 print("当前时间:", datetime.datetime.now())  # 2022-05-17 21:23:56.150028 类型为datetime对象类型
 print("当前日期:", datetime.date.today())
 print('指定时间:', datetime.date(2017, 3, 22))
-print('指定日期时间', datetime.datetime(2017, 3, 22))  # 比datetime.datetime.strptime() 快7倍多
-print('前一天时间', datetime.datetime.now() - datetime.timedelta(days=1, hours=1, minutes=30))
-print('时间段', datetime.timedelta(hours=1, minutes=40) + datetime.timedelta(hours=2, minutes=20))
-print('转为时间戳', datetime.datetime.now().timestamp())
+print('指定日期时间:', datetime.datetime(2017, 3, 22))  # 比datetime.datetime.strptime() 快7倍多
+print('前一天时间:', datetime.datetime.now() - datetime.timedelta(days=1, hours=1, minutes=30))
+print('时间段:', datetime.timedelta(hours=1, minutes=40) + datetime.timedelta(hours=2, minutes=20))
+print('转为时间戳:', datetime.datetime.now().timestamp())
+print('timetuple:', datetime.datetime.now().timetuple())
 
 # 6.计算时间差
 from dateutil import rrule
@@ -54,5 +55,6 @@ months = rrule.rrule(rrule.MONTHLY, dtstart=date_1, until=date_2).count()  # 月
 years = rrule.rrule(rrule.YEARLY, dtstart=date_1, until=date_2).count()  # 年数差
 # days = rrule.rrule(rrule.DAILY, dtstart=date1, until=date2).count()  # 天数差
 
-dd = "2022-10-11T02:31:23.000Z"
-print('dddddddd',datetime.datetime.strptime(dd, "%Y-%m-%dT%H:%M:%S.000Z"))
+# 7.000Z and RFC1123日期格式
+print('000Z:', datetime.datetime.strptime("2022-10-11T02:31:23.000Z", "%Y-%m-%dT%H:%M:%S.000Z"))
+print('RFC1123日期格式:',format_date_time(time.mktime(datetime.datetime.now().timetuple())))
