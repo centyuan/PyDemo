71d16ec6b38631826228e257526b31c0c058b032
centyuan <yuanhuailin@i-soon.net>
2022-11-03 17:59:32 +0800

    no message

--- python特性/Counter计数-字符元素组成判定.py
+++ python特性/Counter计数-字符元素组成判定.py
@@ -10,9 +10,13 @@ from collections import Counter
 #counter 高性能计数器 对其中元素计数
 colors = ['red', 'blue', 'red', 'green', 'blue', 'blue']
 c = Counter(colors)
+print(c)
 print(dict(c))
 
 # 字符元素组成判定
 if Counter('abcd3') == Counter('3acdb'):
     print(type(Counter('ab')))
     print("True")
+
+ss = '[test33333]攻击静态DOMAIN-域名,并拿到了第一滴血'
+print(ss.split('攻击了'))
