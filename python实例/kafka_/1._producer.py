3be01b2e9ba4e8f271ba308b0d2d585a0061f9e0
centyuan <yuanhuailin@i-soon.net>
2023-02-02 19:33:40 +0800

    update kafka consumer

--- python实例/kafka_/1._producer.py
+++ python实例/kafka_/1._producer.py
@@ -15,11 +15,16 @@ def producer_demo():
     for i in range(0, 3):
         # key,同一个key值,会被送至同一个分区,partition,向分区1发送消息
         # 会创建kafka_demo主题
+        # 1.同步发送
+        # future = producer.send('kafka_demo', {'key1': 'value1'}).add_callback().add_errback() # 添加回调函数
         future = producer.send('kafka_demo', {'key1': 'value1'})
         print("send{}".format(str(i)))
         try:
-            # 监控是否发送成功
-            future.get(timeout=10)
+            # 监控是否发送成功,ack确认,消息可靠生产
+            record_metadata = future.get(timeout=10)
+            print("主题topic:",record_metadata.topic)
+            print("分区partition:",record_metadata.partition)
+            print("偏移量offset:",record_metadata.offset)
         except kafka_errors:
             # 将异常传播轨迹信息转换成字符串
             traceback.format_exc()
