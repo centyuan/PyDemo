ae39e3a03caa60344a56a4d81c5c3dbccf8f1ba9
centyuan <yuanhuailin@i-soon.net>
2023-03-17 17:56:42 +0800

    no message

--- 剑指offer/堆.py
+++ 剑指offer/堆.py
@@ -1,7 +1,3 @@
-#！/usr/bin/python3
-# -*- coding:utf-8 -*-
-# author centyuan
-# @time 19-9-8 下午8:06
 """
 堆 (heap) 是一种经过排序的完全二叉树，其中任一非叶子节点的值均不大于（或不小于）其左孩子和右孩子节点的值。
 
@@ -10,23 +6,23 @@
 最小堆 根结点的键值是所有堆结点键值中最小者。
 """
 
-class heap(object):
 
+class heap(object):
 
     def __init__(self):
         # 初始化一个空堆，使用数组来在存放堆元素，节省存储
         self.data_list = []
 
-    def get_parent_index(self,index):
-        #返回父节点的下标
-        if index == 0 or index > len(self.data_list)-1:
-            return  None
+    def get_parent_index(self, index):
+        # 返回父节点的下标
+        if index == 0 or index > len(self.data_list) - 1:
+            return None
         else:
-            return  (index-1) >> 1 #二进制形式右移1位
+            return (index - 1) >> 1  # 二进制形式右移1位
 
-    def swap(self,index_a,index_b):
-        #交换数组中两个元素
-        self.data_list[index_a],self.data_list[index_b] = self.data_list[index_b],self.data_list[index_a]
+    def swap(self, index_a, index_b):
+        # 交换数组中两个元素
+        self.data_list[index_a], self.data_list[index_b] = self.data_list[index_b], self.data_list[index_a]
 
     def insert(self, data):
         # 先把元素放在最后，然后从后往前依次堆化
@@ -42,28 +38,29 @@ class heap(object):
             parent = self.get_parent_index(parent)
 
     def removeMax(self):
-        #删除堆顶元素，然后将最后一个元素放在堆顶，再从上往下依次堆化
+        # 删除堆顶元素，然后将最后一个元素放在堆顶，再从上往下依次堆化d
         remove_data = self.data_list[0]
         self.data_list[0] = self.data_list[-1]
         del self.data_list[-1]
 
-        #堆化
+        # 堆化
         self.heapify(0)
         return remove_data
 
-    def heapify(self,index):
-        #从上往下堆化，从index 开始堆化操作 (大顶堆)
-        total_index = len(self.data_list) -1
+    def heapify(self, index):
+        # 从上往下堆化，从index 开始堆化操作 (大顶堆)
+        total_index = len(self.data_list) - 1
         while True:
             maxvalue_index = index
-            if 2*index +1 <=  total_index and self.data_list[2*index +1] > self.data_list[maxvalue_index]:
-                maxvalue_index = 2*index +1
-            if 2*index +2 <=  total_index and self.data_list[2*index +2] > self.data_list[maxvalue_index]:
-                maxvalue_index = 2*index +2
+            if 2 * index + 1 <= total_index and self.data_list[2 * index + 1] > self.data_list[maxvalue_index]:
+                maxvalue_index = 2 * index + 1
+            if 2 * index + 2 <= total_index and self.data_list[2 * index + 2] > self.data_list[maxvalue_index]:
+                maxvalue_index = 2 * index + 2
             if maxvalue_index == index:
                 break
-            self.swap(index,maxvalue_index)
+            self.swap(index, maxvalue_index)
             index = maxvalue_index
+
 
 """
 请将 元素 1-10 放进堆，并展示建堆情况，及删除堆顶元素情况
@@ -76,6 +73,6 @@ class heap(object):
 if __name__ == "__main__":
     myheap = heap()
     for i in range(10):
-        myheap.insert(i+1)
-    print('建堆:',myheap.data_list)
+        myheap.insert(i + 1)
+    print('建堆:', myheap.data_list)
     print("删除堆顶元素：", [myheap.removeMax() for _ in range(10)])
