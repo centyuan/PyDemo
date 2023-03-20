83ba0411bb54e07604de51b62fd9efdae3e69ea4
centyuan <yuanhuailin@i-soon.net>
2023-03-20 10:00:56 +0800

    no message

--- python特性/生成6位数字密码字典.py
+++ python特性/生成6位数字密码字典.py
@@ -2,10 +2,16 @@ import itertools as its
 
 # words = "abcdefghijklmnopqrstuvwxyz1234567890"
 words = '1234567890'
-r = its.product(words,repeat=6)
-dic = open("pass.txt", "a")    #保存
-for i in r:
-    dic.write("".join(i))
-    dic.write("".join("\r"))
-dic.close()
-
+dict_f = {"f" + str(i): open("pass" + str(i) + ".txt", "a") for i in range(0, 10)}
+r = its.product(words, repeat=6)
+for item in r:
+    f = None
+    for i in range(0, 10):
+        if int(item[0]) == i:
+            f = dict_f["f" + str(i)]
+            break
+    if f:
+        f.write("".join(item))
+        f.write("".join("\r"))
+    else:
+        break
