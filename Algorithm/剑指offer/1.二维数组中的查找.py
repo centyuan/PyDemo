e3a2a51dc8a3264c93b3c94b34c4186593857db6
centyuan <yuanhuailin@i-soon.net>
2023-03-24 18:17:47 +0800

    no message

--- Algorithm/剑指offer/1.二维数组中的查找.py
+++ Algorithm/剑指offer/1.二维数组中的查找.py
@@ -1,13 +1,24 @@
+import bisect
+from typing import List
+
+
 class Solution:
     # 1.直接查找 O(nm)
-    def find(self, target, array):
-        x, y = 0, len(array[0]) - 1
-        while x <= len(array) - 1 and y >= 0:
-            if array[x][y] == target:
+    def findNumberIn2DArray(self, matrix: List[int], target: int) -> bool:
+        x, y = 0, len(matrix[0]) - 1
+        while x <= len(matrix) - 1 and y >= 0:
+            if matrix[x][y] == target:
                 return True
-            elif array[x][y] > target:
+            elif matrix[x][y] > target:
                 y -= 1
             else:
                 x += 1
         return False
-    # 2.二分查找:对每一行使用二分查找
+
+    # 2.二分查找:对每一行使用二分查找 O(nlogm)
+    def findNumberIn2DArray_2(self, matrix: List[int], target: int) -> bool:
+        for row in matrix:
+            id_x = bisect.bisect_left(row, target)  # 没找到返r回插入的位置
+            if id_x < len(row) and row[id_x] == target:
+                return True
+        return False
--- Testing.py
+++ Testing.py
@@ -1,10 +1,6 @@
-# 接受客户端信息并处理，这里只是简单把客户都发来的返回去
-async def recv_user_msg(websocket):
-    while True:
-        recv_text = await websocket.recv()
-        print('接收消息:', websocket.pong, recv_text)
-        response_text = f'服务器返回:{recv_text}'
-        print('返回消息:', response_text)
-        await websocket.send(response_text)
-
-recv_user_msg()
+s = "Talk is cheap show me the code"
+print(s.upper())
+print("原始",s)
+print(s.split(" "))
+print("原始",s)
+print(s.isspace())
