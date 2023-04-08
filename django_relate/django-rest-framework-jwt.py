d4936d0f1b0a5a916a235e327597713fef71cd4e
DESKTOP-2NGFHBO\cent <centyuan@outlook.com>
2023-04-08 21:08:39 +0800

    no message

--- django_relate/django-rest-framework-jwt.py
+++ django_relate/django-rest-framework-jwt.py
@@ -1,8 +1,18 @@
-# 官方文档
-# https://jpadilla.github.io/django-rest-framework-jwt/#additional-settings
-# Django-DRF框架中认证与权限
-# https://blog.51cto.com/u_15127659/4679882
+"""
+官方文档
+https://jpadilla.github.io/django-rest-framework-jwt/#additional-settings
+Django-DRF框架中认证与权限
+https://blog.51cto.com/u_15127659/4679882
+JWT:json web token是无状态的,采用对称加密(),只需验证签名是否是自己签发的
+有三部分组成:header.payload.signature
+1.header:由token类型(JWT),算法名称(HS256),在base64
+header = base64.b64encode({"alg":"HS256","typ":"JWT"}.encode("utf-8")
+2.payload:记录存储简单的不重要的信息(信息是公开的),例如(用户名,id,过期时间)等,在base64
+3.signature:用于验证签名信息算法被篡改
+data = header的base64编码+payload的base64编码+secret(salt)
+signature = 加密方式(data)
 
+"""
 # 生成token
 from rest_framework_jwt.settings import api_settings
 
