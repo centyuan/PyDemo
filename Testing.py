1d2926875618fafc538b61825ff86a60f2b5e1fe
DESKTOP-2NGFHBO\cent <centyuan@outlook.com>
2023-04-24 23:07:13 +0800

    no message

--- Testing.py
+++ Testing.py
@@ -5,34 +5,41 @@ from elasticsearch_dsl import Document, Date, Nested, Boolean, analyzer, InnerDo
 from elasticsearch_dsl.connections import connections
 
 # connections.create_connection(hosts=["123.60.180.204:9200"])
-es = connections.create_connection(hosts=["123.60.180.204:9200"], timeout=20)
+es = connections.create_connection(hosts=["123.60.180.204:9200"], timeout=60)
 
 
 class Article(Document):
-    title = Text(fields={'title': Keyword()})
-    author = Text()
+    title = Text(analyzer="snowball", fields={"raw": Keyword()})
+    body = Text(analyzer="snowball")
+    tags = Keyword()
+    published_from = Date()
+    lines = Integer()
 
     class Index:
-        name = 'myindex'  # 索引名
+        name = 'my_article'  # 索引名
+
+    def save(self, **kwargs):
+        self.lines = len(self.body.split())
+        return super().save(**kwargs)
 
 
 if __name__ == '__main__':
-    # Article.init()  # 创建映射
+    Article.init(using=es)  # 创建索引映射
+    article = Article(meta={'id': 42}, title='Hello world!', tags=['test'])
+    article.body = "长文档"
+    article.published_from = datetime.now()
+    article.save()
     # # 保存数据
     # article = Article()
     # article.title = "test"
     # article.author = "lxx"
     # article.save()  # 保存数据
-    res = Search(using=es).index("teacher").query({"match":{"description":"可"}})
-    res2 = res.query().source(["name"]).execute()
-    for data in res:
-        print(data.to_dict())
-    index_name = es.cat.indices(format="json", h="index")
-    # # 查询数据
-    # s = Article.search()
-    # s = s.filter('match', title="test")
-    # results = s.execute()
-    # print(results)
+
+    # 查询数据
+    s = Article.search()
+    s = s.filter('match', title="Hello world!")
+    results = s.execute()
+    print(results)
     #
     # # 删除数据
     # s = Article.search()
@@ -44,3 +51,8 @@ if __name__ == '__main__':
     # print(results[0])
     # results[0].title = "xxx"
     # results[0].save()
+    # res = Search(using=es).index("teacher").query({"match": {"description": "可"}})
+    # res2 = res.query().source(["name"]).execute()
+    # for data in res:
+    #     print(data.to_dict())
+    # index_name = es.cat.indices(format="json", h="index")
