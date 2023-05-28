81a64ded221487e7d3b02ffae87fc3d6f5279f7b
DESKTOP-2NGFHBO\cent <centyuan@outlook.com>
2023-05-28 11:00:04 +0800

    sqlalchemy session

--- python_example/SQLAlchemy_example.py
+++ python_example/SQLAlchemy_example.py
@@ -5,7 +5,10 @@ from sqlalchemy import Column, Integer, String, DateTime, create_engine
 from sqlalchemy.orm import sessionmaker
 
 ModelBase = declarative_base()  # 元类
-
+"""
+https://www.cnblogs.com/ChangAn223/p/11277468.html
+https://www.cnblogs.com/ybjourney/p/11876595.html
+"""
 
 # 1.ORM使用模式(先在数据库建好对应表)或使用ModelBase.metadata.create_all(engine)
 class User(ModelBase):
@@ -17,12 +20,25 @@ class User(ModelBase):
 
 
 # 2.初始化数据库连接
-engine = create_engine('mysql+pymysql://root:123456@localhost:3306/runoob')
+# 创建一个数据库引擎方便session建立会话
+engine = create_engine('mysql+pymysql://root:123456@localhost:3306/runoob',
+                       echo=True,
+                       pool_size=100,
+                       pool_recycle=3600, # 一个小时
+                       pool_pre_ping=True,
+
+                       )
+"""
+pool_size:连接池大小
+pool_recycle:指定时间内回收连接,必须小于mysql wait_timeout
+pool_pre_ping:每次从连接池取走一个连接,都会向数据库请求select 1,测试查询语句来判断服务器是否正常运行。当该连接出现 disconnect 的情况时，该连接连同pool中的其它连接都会被回收。
+
+"""
 # 3.创建表
 # ModelBase.metadata.create_all(engine)
-# 4.创建DBsession
+# 4.创建DBsession,一个工厂类,绑定了引擎
 DBsession = sessionmaker(bind=engine)
-# 5.创建session对象
+# 5.创建session对象,连接池会分配一个connection,当调用session.close(),将连接归还给连接池而不会关闭连接
 session = DBsession()
 # 6.添加数据
 # new_user = User(id=1, date_joined=datetime.datetime.now(), username="袁", password="密码")
--- python特性/上下文管理器.py
+++ python特性/上下文管理器.py
@@ -1,7 +1,13 @@
 """
 上下文管理器类
-类实现了__enter__和__exit__,这个类的实例就是个上下文管理器,可以用with管理
+1.类实现了__enter__和__exit__,这个类的实例就是个上下文管理器,可以用with管理
+2.通过装饰器,contextlib.contextmanager
 
+__enter__:用于赋值给as后面的变量,
+__exit__:清理正在使用的资源,且还包含了用于捕获异常
+    exc_type:异常类型
+    exc_value:异常值
+    exc_tab:异常的位置(错误栈)
 """
 
 
@@ -10,6 +16,12 @@ class CTManager:
         print("__enter__被执行")
 
     def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+
+        @param exc_type:
+        @param exc_val:
+        @param exc_tb:
+        """
         print("__exit__被执行")
 
 
