871131fda943b6a451295df3dc2d082888270e33
centyuan <centyuan@outlook.com>
2023-07-07 02:09:51 +0000

    update python_example/kafka_sample/1._producer.py.
    Signed-off-by: centyuan <centyuan@outlook.com>

--- python_example/kafka_sample/1._producer.py
+++ python_example/kafka_sample/1._producer.py
@@ -1,34 +1,73 @@
-from kafka import KafkaProducer, KafkaConsumer
+"""
+pip install kafka-python
+"""
+import json 
+from kafka import KafkaProducer
+from datetime import datetime
+from typing import List 
 from kafka.errors import kafka_errors
-import traceback
-import json
 
+# https://zhuanlan.zhihu.com/p/637683728
+#batch_size: 批次大小,默认64kb,生产者将发送到相同分区的多个信息打包为一个批次,为0表示禁用
+#linger.ms: 消息停留时间,到这个时间后必须发送
+"""
+场景1消息需要按顺序发送并且只能在一个partation上:
+    retries:重试次数
+    max_in_flight_requests_per_connection=1:控制生产者在收到服务器响应之前只能发送一个消息
 
-def producer_demo():
-    # 1.发送json格式数据
-    producer = KafkaProducer(
-        bootstrap_servers=['43.136.217.222:9092'],
-        key_serializer=lambda k: json.dumps(k).encode(),
-        value_serializer=lambda v: json.dumps(v).encode()
-    )
-    # 2.发送三条消息
-    for i in range(0, 3):
-        # key,同一个key值,会被送至同一个分区,partition,向分区1发送消息
-        # 会创建kafka_demo主题
-        # 1.同步发送
-        # future = producer.send('kafka_demo', {'key1': 'value1'}).add_callback().add_errback() # 添加回调函数
-        future = producer.send('kafka_demo', {'key1': 'value1'})
-        print("send{}".format(str(i)))
-        try:
-            # 监控是否发送成功,ack确认,消息可靠生产
-            record_metadata = future.get(timeout=10)
-            print("主题topic:",record_metadata.topic)
-            print("分区partition:",record_metadata.partition)
-            print("偏移量offset:",record_metadata.offset)
-        except kafka_errors:
-            # 将异常传播轨迹信息转换成字符串
-            traceback.format_exc()
+场景2只关心吞吐量-容许少量消息发送失败，不关注消息顺序
+    异步发送+acks=0
 
+场景3需要知道消息是否发送成功,不关注消息顺序
+    异步发送+回调
+    retries=0 
+"""
 
-if __name__ == '__main__':
-    producer_demo()
+class MyProducer():
+    def __init__(self, bootstrap_servers:str=None,topic:str=None):
+        self.bootstrap_servers = bootstrap_servers if bootstrap_servers else "127.0.0.1:9092"
+        self.topic = topic if topic else "notify_topic"
+        self.producer = KafkaProducer(
+            value_serializer=lambda v:json.dumps(v).encode("utf-8"),
+            key_serialzier=lambda v:json.dumps(v).encode("utf-8"),
+            api_version=(0,10,1),
+            batch_size=0,
+        )
+    
+    # 1.同步发送
+    def send_msg(self, msg:str="你好", partition:int=0):
+        future = self.producer.send(self.topic, msg, partition=partition)
+        # future.success,future.succeded()
+        if future.failed():
+            print("发送失败")
+            raise Exception(f"发送失败:{future.exception}")
+        metadata_ = future.get(timeout=30)  # 同步发送,等待消息的信息
+        print("消息分区:",metadata_.partition)
+        print("消息offset:",metadata_.offset)
+    
+    # 2.异步发送,不关心消息是否到达
+    def asyn_send_msg(self, msg:str="你好", partition:int=0):
+        for item in range(5):
+            self.producer.send(self.topic, msg, partition=partition)
+        self.producer.flush()  # 异步发送,批量提交
+    
+    # 3.异步发送+回调,消息发送成功会回调
+    def asyn_send_msg_callback(self, msg:str="你好",partition:int=0):
+        for item in range(5):
+            future = self.producer.send(self.topic, msg, partition=partition)
+            future.add_callback(self.send_success)
+            future.add_errback(self.send_error)
+
+    def send_success(self):
+        print("send_success:发送成功")
+        return
+    
+    def send_error(self):
+        print("send_error:发送失败")
+        return 
+    
+if __name__ == "__main__":
+    broker = "10.24.2.2:9092"
+    topic = "notify_topic"
+    kafka_pro = MyProducer(bootstrap_servers=broker,topic=topic)
+    kafka_pro.send("hello world")
