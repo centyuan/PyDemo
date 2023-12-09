2bd93a1510f261dd55f7c717e5bce91c51b5111c
DESKTOP-2NGFHBO\cent <centyuan@outlook.com>
2023-12-09 15:40:50 +0800

    docs: update moudle_logging

--- PyNote/moudle_logging.py
+++ PyNote/moudle_logging.py
@@ -2,24 +2,35 @@
 # Author centyuan
 # Date 2019/11/20 11:16
 
+
+
+"""
+format常用格式说明
+%(levelno)s: 打印日志级别的数值
+%(levelname)s: 打印日志级别名称
+%(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
+%(filename)s: 打印当前执行程序名
+%(funcName)s: 打印日志的当前函数
+%(lineno)d: 打印日志的当前行号
+%(asctime)s: 打印日志的时间
+%(thread)d: 打印线程ID
+%(threadName)s: 打印线程名称
+%(process)d: 打印进程ID
+%(message)s: 打印日志信息
+
+"""
+
+# 1.日志输出到控制台
 import logging
 import os
 
-logging.debug("苍井空")
-logging.info("麻生希")
-logging.warning("小泽玛利亚")  # 默认生成的root logger的level是logging.WARNING,低于该级别的就不输出了
-logging.error("桃谷绘里香")
-logging.critical("泷泽萝拉")
-
-# 1.日志输出到控制台
-import logging  # 引入logging模块  # noqa: E402
-
+# logging.basicConfig函数进行配置了日志级别和日志内容输出格式；因为级别为DEBUG
+# 所以会将DEBUG级别以上的信息都输出显示再控制台上。
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s",
-)  # logging.basicConfig函数对日志的输出格式及方式做相关配置
-# logging.basicConfig函数进行配置了日志级别和日志内容输出格式；因为级别为DEBUG
-# 所以会将DEBUG级别以上的信息都输出显示再控制台上。
+)  
+
 logging.info("this is a loggging info message")
 logging.debug("this is a loggging debug message")
 logging.warning("this is loggging a warning message")
@@ -31,19 +42,22 @@ import logging  # noqa: E402
 
 # 1 创建一个logger
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-log_path = ""
-log_file = log_path + "logging_test.log"
+# logger.setLevel(logging.INFO)
+
+
 # 2 创建一个handler，用于写入日志文件
-fh = logging.FileHandler(log_file)
+fh = logging.FileHandler("logging_test.log")
 fh.setLevel(logging.DEBUG)  # 设置日志级别
+
 # 3 定义handler的输出格式
-formatter = logging.Formatter(
+log_formatter = logging.Formatter(
     "%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s"
 )
-fh.setFormatter(formatter)  # 设置Formatter
+fh.setFormatter(log_formatter)  # 设置Formatter
+
 # 4 将logger添加到handler里面
 logger.addHandler(fh)  # 添加一个Handler logger.removeHanlder() 删除一个handler
+
 # 5 traceback模块被用于跟踪异常返回信息，可以在logging中记录下traceback，
 
 try:
@@ -61,26 +75,9 @@ except Exception:
 
 logger.info("Finish")
 
-"""
-6、format常用格式说明
-%(levelno)s: 打印日志级别的数值
-%(levelname)s: 打印日志级别名称
-%(pathname)s: 打印当前执行程序的路径，其实就是sys.argv[0]
-%(filename)s: 打印当前执行程序名
-%(funcName)s: 打印日志的当前函数
-%(lineno)d: 打印日志的当前行号
-%(asctime)s: 打印日志的时间
-%(thread)d: 打印线程ID
-%(threadName)s: 打印线程名称
-%(process)d: 打印进程ID
-%(message)s: 打印日志信息
-
-"""
 
 # 3. logging项目使用
 from logging.handlers import RotatingFileHandler  # noqa: E402
-
-# MemoryHandler 输出到内存指定buffer
 
 
 class Logger:
@@ -91,31 +88,71 @@ class Logger:
             cls.__instance = object.__new__(cls)
         return cls.__instance
 
-    def __init__(self, log_name: str = "application.log", log_level=logging.DEBUG):
-        path = os.getcwd() + os.sep + "logs"
-        fmt = logging.Formatter(
-            "[%(asctime)s.%(msecs)03d][%(levelname)s][%(filename)s][%(lineno)d] %(message)s",
-            "%Y-%m-%d %H:%M:%S",
+    def __init__(self,
+             log_path:str = None, 
+             log_name: str = "access.log", 
+             log_level = logging.DEBUG,
+             To_console = True,
+             To_file = False,
+             To_ES = False,
+             ROTA = False,
+             ):
+        if not log_path:
+            log_path = os.getcwd() + os.sep + "logs"+os.sep # os.sep 路径分隔符(Linux使用/,window使用\)
+
+      
+        log_formatter = logging.Formatter(
+            fmt=" %(asctime)s.%(msecs)03d  |  %(levelname)s  |  %(filename)s |  %(lineno)d  |  %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
         )
-        self.logger = logging.getLogger(path)
+        self.logger = logging.getLogger()
         self.logger.setLevel(log_level)
-        # 流handler 输出到sys.stdout,sys.stderr输出到控制台
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(fmt)
-        # RotatingFileHandler 循环日志文件
-        rotate_handler = RotatingFileHandler(
-            path + os.sep + log_name, maxBytes=1024 * 1024 * 50, backupCount=5
-        )
+
+        # 1.console流handler 输出到sys.stdout,sys.stderr输出到控制台
+        if To_console:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(log_formatter)
+            self.logger.addHandler(stream_handler)
+        
+        # 2.文件
+        if To_file:
+            #self.logger = logging.getLogger(log_path)
+            if ROTA: # RotatingFileHandler 循环日志文件
+                rota_log = log_path+"rotate_"+log_name
+                if not os.path.exists(rota_log):
+                    os.makedirs(rota_log)
+                rotate_handler = RotatingFileHandler(rota_log, maxBytes=1024 * 1024 * 50, backupCount=5)
+                # maxBytes 最大字节数,backupCount 超过最大字节数保留5个
+                self.logger.addHandler(rotate_handler)
+            else:
+                log_file = log_path+log_name
+                if not os.path.exists(log_file):
+                    os.makedirs(log_file)
+                fh = logging.FileHandler(log_file)
+                fh.setFormatter(log_formatter)
+                self.logger.addHandler(fh)
         # TimedRotatingFileHandler 定时生成新日志文件
-        self.logger.addHandler(stream_handler)
-        self.logger.addHandler(rotate_handler)
+
+        # 3. ES
+        if To_ES:
+            cm_handler = CMRESHandler(
+                hosts=[{"host": "localhost", "port": 9200}],
+                es_index_name="log_index",
+                auth_type=CMRESHandler.AuthType.NO_AUTH,
+            )
+            cm_handler.setFormatter(log_formatter)
+            self.logger.addHandler(cm_handler)
+
+    
+     
 
     def get_logger(self):
         return self.logger
 
 
-logger = Logger.get_logger()
+logger = Logger().get_logger()
 logger.info("日志")
+
 
 # 4.日志写入ES
 """
